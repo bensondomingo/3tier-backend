3554ae3a933e09fdf0b62bcb561904d13f8b3f75
Benson Domingo <benson.domingo@outlook.com>
2021-03-24 03:23:45 +0800

    Add cors

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 const express = require('express')
-const path = require('path')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const usersRouter = require('./routes/users')
-
 const app = express()
+
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
 
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
-
+app.use(cors(corsOptions))
 app.use('/users', usersRouter)
 
 const startApp = async () => {
